f81f635eb601a6ccbe83cda1f2db12a47e8b457c
edvantupinamba <ti.edvan@gmail.com>
2022-09-06 11:30:46 -0300

    Update Ola_mundo.py

--- Ola_mundo.py
+++ Ola_mundo.py
@@ -1,7 +1,14 @@
 # Mostre na tela a mensagem Olá mundo!
-
 # Isso pode ser feito de várias formas vou mostrar nesse exeplo algo simples e prático usando a função print().
-
 # print() - é utilizado para saida de dados.
 
 print("Olá Mundo!")
+
+#Agora uma saida de dados utilizando uma variável
+
+saudacao = "Olá Mundo"
+
+print(saudacao)
+
+''' Perceba que no prieiro print foi utilizado aspas e no segundo sem aspas, por quê? Por que no segundo print estamos chamando a variavel dentro da função print().
+'''
