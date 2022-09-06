5fae397366204271965dc32f10395305fd91591a
edvantupinamba <ti.edvan@gmail.com>
2022-09-06 11:17:07 -0300

    Update Ola_mundo.py

--- Ola_mundo.py
+++ Ola_mundo.py
@@ -1,1 +1,7 @@
+# Mostre na tela a mensagem Olá mundo!
+
+# Isso pode ser feito de várias formas vou mostrar nesse exeplo algo simples e prático usando a função print().
+
+# print() - é utilizado para saida de dados.
+
 print("Olá Mundo!")
